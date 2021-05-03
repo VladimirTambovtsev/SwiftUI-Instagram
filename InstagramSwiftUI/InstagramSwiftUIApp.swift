5af3d4791fab35c29ed0064046ce03e02bb4aa3d
Vladimir Tambovtsev <tambovcev99@mail.ru>
2021-05-03 17:06:19 +0300

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by Владимир on 03.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
